b8c4f0db0cec33a01117119297678bc4c26abbb4
hesam-init <hesam.init@gmail.com>
2024-04-15 22:27:07 +0330

    init: dot files

---
+++ shells/zsh/.zsh/aliases/export.zsh
@@ -0,0 +1,5 @@
+source ~/.zsh/aliases/global/main.zsh
+source ~/.zsh/aliases/directories/main.zsh
+source ~/.zsh/aliases/git/main.zsh
+source ~/.zsh/aliases/network/main.zsh
+source ~/.zsh/aliases/flatpak/main.zsh
