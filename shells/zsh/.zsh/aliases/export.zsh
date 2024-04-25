123b60f795a44c29d933b5418c95305d8c118bfa
hesam-init <hesam.init@gmail.com>
2024-04-25 13:04:22 +0330

    feat: vscode download alias

--- shells/zsh/.zsh/aliases/export.zsh
+++ shells/zsh/.zsh/aliases/export.zsh
@@ -3,3 +3,4 @@ source ~/.zsh/aliases/directories/main.zsh
 source ~/.zsh/aliases/git/main.zsh
 source ~/.zsh/aliases/network/main.zsh
 source ~/.zsh/aliases/flatpak/main.zsh
+source ~/.zsh/aliases/vscode/main.zsh
---
+++ shells/zsh/.zsh/aliases/vscode/main.zsh
@@ -0,0 +1,4 @@
+CODE_DOWNLOAD_URL="https://code.visualstudio.com/sha/download\?build\=stable\&os\=linux-x64"
+
+alias code="$CODE_PATH --no-sandbox"
+alias codeup="curl -O -L -J $CODE_DOWNLOAD_URL"
