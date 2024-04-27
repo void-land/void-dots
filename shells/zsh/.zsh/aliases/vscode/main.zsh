a0e6c74ecd86476be54f502b251060462103f85b
hesam-init <hesam.init@gmail.com>
2024-04-27 22:42:21 +0330

    feat: vscode updater

--- shells/zsh/.zsh/aliases/vscode/main.zsh
+++ shells/zsh/.zsh/aliases/vscode/main.zsh
@@ -1,4 +1,25 @@
 CODE_DOWNLOAD_URL="https://code.visualstudio.com/sha/download\?build\=stable\&os\=linux-x64"
+CODE_DOWNLOAD_PATH="/tmp/vscode.tar.gz"
+CODE_TMP_FOLDER="/tmp/VSCode-linux-x64"
 
 alias code="$CODE_PATH --no-sandbox"
-alias codeup="curl -O -L -J $CODE_DOWNLOAD_URL"
+alias codeupdate="curl -o $CODE_DOWNLOAD_PATH -L --retry 3 $CODE_DOWNLOAD_URL"
+
+codeup() {
+    codeupdate
+
+    if [ -f $CODE_DOWNLOAD_PATH ]; then
+        tar -xvf $CODE_DOWNLOAD_PATH -C /tmp
+
+        if [ -d $CODE_TMP_FOLDER ]; then
+            mv $CODE_TMP_FOLDER /tmp/vscode
+        fi
+
+        sudo rm -rf /opt/vscode
+        sudo mv /tmp/vscode /opt
+
+        echo "Code updated successfully!"
+    else
+        echo "Error: File $CODE_DOWNLOAD_PATH does not exist."
+    fi
+}
