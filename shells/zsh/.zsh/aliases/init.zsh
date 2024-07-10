d96b24b963641ebc0c012f519bbd55309b5d6b62
hesam-init <hesam.init@gmail.com>
2024-07-10 19:27:10 +0330

    chore: zsh alias structure

--- shells/zsh/.zsh/aliases/init.zsh
+++ shells/zsh/.zsh/aliases/init.zsh
@@ -1,10 +1,16 @@
+source ~/.zsh/aliases/_flatpak.zsh
+source ~/.zsh/aliases/_git.zsh
+source ~/.zsh/aliases/_network.zsh
+source ~/.zsh/aliases/_vscode.zsh
+source ~/.zsh/aliases/_brightness.zsh
+
+alias dotfiles="cd $DOTFILES && ls"
+alias projects="cd $HOME/Code && ls"
+
 alias alist="alias"
-
 alias fetch="neofetch"
-
 alias wget-scrape="wget -m -k -K -E"
 alias pcp="pwd | wl-copy && echo 'Path : $(pwd) copied to clipboard !'"
-
 alias grub-path="cd /etc/default"
 alias findbin="whereis"
 alias pmx="chmod +x"
@@ -25,7 +31,6 @@ alias psfind="ps -aux | grep"
 alias dsu="dust -d 1 -b"
 alias edit-dns="sudo nano /etc/resolv.conf"
 alias dns-changer="sudo $DNS_CHANGER"
-
 alias lt="eza --header --icons -l"
 alias lf="eza -lF --color=always"
 alias ld="eza -lD --header"
--- shells/zsh/.zshrc
+++ shells/zsh/.zshrc
@@ -4,7 +4,7 @@ source $ZSH/oh-my-zsh.sh
 
 source ~/.zsh/_keybinds.zsh
 source ~/.zsh/functions/init.zsh
-source ~/.zsh/aliases/export.zsh
+source ~/.zsh/aliases/init.zsh
 source ~/.zsh/os/init.zsh
 
 eval "$(atuin init zsh --disable-up-arrow)"
