904aa8d7aaba7f6e757d56dbccb0ffb27a820889
hesam-init <hesam.init@gmail.com>
2024-08-16 19:04:35 +0330

    fix: tide config sync

---
+++ shells/fish/conf.d/tide.fish
@@ -0,0 +1,1 @@
+tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Sparse --icons='Few icons' --transient=No
