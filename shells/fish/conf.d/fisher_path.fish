7fafb8e356d6f068006abd5a0a5f29da7828abdd
hesam-init <hesam.init@gmail.com>
2024-08-16 19:58:14 +0330

    fix: fisher plugin

--- shells/fish/conf.d/fisher_path.fish
+++ shells/fish/conf.d/fisher_path.fish
@@ -1,18 +1,10 @@
-set -x fisher_path $__fish_config_dir/plugins
+set fisher_path $__fish_config_dir/plugins
 
-set --query _fisher_path_initialized && exit
-set --global _fisher_path_initialized
+set --query fisher_path || exit
 
-if test -z "$fisher_path" || test "$fisher_path" = "$__fish_config_dir"
-    exit
-end
-
-set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
-set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 
 for file in $fisher_path/conf.d/*.fish
-    if ! test -f (string replace --regex "^.*/" $__fish_config_dir/conf.d/ -- $file)
-        and test -f $file && test -r $file
-        source $file
-    end
+    source $file
 end
