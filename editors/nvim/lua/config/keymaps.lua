e1768f1a59f04e7cfe41bdba7510b8a27f5cfcd9
hesam-init <hesam.init@gmail.com>
2024-09-25 01:48:59 +0330

    feat: base navigation keymaps for neovim

--- editors/nvim/lua/config/keymaps.lua
+++ editors/nvim/lua/config/keymaps.lua
@@ -5,11 +5,32 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-keymap.set("n", "<C-l>", "V$", opts)
+-- Function to extend opts with description
+local function with_desc(desc)
+  return vim.tbl_extend("force", opts, { desc = desc })
+end
 
-keymap.set("n", "<C-p>", ":Telescope find_files<CR>", opts)
+-- General keymaps
+keymap.set("n", "<C-a>", "ggVG", with_desc("Select all"))
+keymap.set("n", "<C-l>", "V$", with_desc("Select to end of line"))
+keymap.set("n", "<C-p>", ":Telescope find_files<CR>", with_desc("Find files"))
+keymap.set("n", "<C-CR>", "o<Esc>", with_desc("Insert new line below"))
+keymap.set("n", "<C-S-CR>", "i", with_desc("Enter insert mode"))
+keymap.set("i", "<C-S-CR>", "<Esc>", with_desc("Exit insert mode"))
 
-keymap.set("n", "<C-CR>", "o<Esc>", opts)
+-- Window Navigation
+keymap.set("n", "<A-Left>", "<C-w>h", with_desc("Move to left window"))
+keymap.set("n", "<A-Down>", "<C-w>j", with_desc("Move to window below"))
+keymap.set("n", "<A-Up>", "<C-w>k", with_desc("Move to window above"))
+keymap.set("n", "<A-Right>", "<C-w>l", with_desc("Move to right window"))
 
-keymap.set("n", "<C-S-CR>", "i", opts)
-keymap.set("i", "<C-S-CR>", "<Esc>", opts)
+-- Undo and Redo
+keymap.set({ "n", "i" }, "<C-z>", "<C-o>u", with_desc("Undo"))
+keymap.set("n", "<C-y>", "<C-r>", with_desc("Redo"))
+
+-- Telescope
+keymap.set("n", "<C-s-f>", ":Telescope live_grep<CR>", with_desc("Search in files"))
+
+-- Copy and Paste
+keymap.set("v", "<C-c>", "y", with_desc("Copy selected items"))
+keymap.set("v", "<C-x>", "d", with_desc("Cut selected items"))
