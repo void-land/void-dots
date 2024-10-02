209e63f165140c2df2878ee4f1b6f287cee8cca8
hesam-init <hesam.init@gmail.com>
2024-10-03 01:54:03 +0330

    chore: fish shell new shortcuts

--- shells/fish/aliases/_shortcuts.fish
+++ shells/fish/aliases/_shortcuts.fish
@@ -1,6 +1,17 @@
-abbr dsu "dust -d 1 -b"
 abbr jar "java -jar"
-abbr cpd "pwd | wl-copy"
-abbr pg "ping google.com"
-abbr ins "grep -E 'sse3|sse4|avx|avx2' /proc/cpuinfo"
+
 abbr svpn "sudo vpn"
+abbr pg "ping google.com"
+
+abbr killer "pkill -f"
+abbr psfind "ps -aux | grep"
+
+abbr dfl "df -h"
+abbr dux "du -sh *"
+abbr dus "dust -d 1 -b"
+abbr duse "du -sh"
+
+abbr cpd "pwd | wl-copy"
+abbr srm "sudo rm"
+abbr rf "rm -rf"
+abbr ins "grep -E 'sse3|sse4|avx|avx2' /proc/cpuinfo"
