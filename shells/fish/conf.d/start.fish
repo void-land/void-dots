5ac0c02566f2b78f08fedb4b66a83ca92691cafe
hesam-init <hesam.init@gmail.com>
2024-11-10 22:59:30 +0330

    fix: hyprland tty1 logout from fish shell

--- shells/fish/conf.d/start.fish
+++ shells/fish/conf.d/start.fish
@@ -1,3 +1,7 @@
-if test (tty) = /dev/tty1
-    dbus-run-session Hyprland
+if test (tty) = "/dev/tty1"
+    if not set -q HYPRLAND_RUNNING
+        set -g HYPRLAND_RUNNING 1
+        
+        exec dbus-run-session Hyprland
+    end
 end
