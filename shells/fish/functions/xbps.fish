2c032aa65dcffebdb6ff2e7074008d16f54da75c
hesam-init <hesam.init@gmail.com>
2024-12-05 19:46:59 +0330

    refactor: xbps helper

--- shells/fish/functions/xbps.fish
+++ shells/fish/functions/xbps.fish
@@ -1,42 +1,18 @@
-function xbps --argument-names sub_command -d "Short and friendly command wrapper for XBPS"
+function xbps -d "Short and friendly command wrapper for XBPS"
     if begin
             not set -q argv[1]; or contains -- -h $argv; or contains -- --help $argv
         end
-        echo "Usage: xbps COMMAND [OPTIONS] [arg...]"
-        echo "Commands:"
-        echo "  pkgs             Change directory to VOID_PACKAGES_PATH"
-        echo "  shutdown         Shutdown the system immediately"
-        echo "  reboot           Reboot the system immediately"
-        echo "  repos            List XBPS repositories"
-        echo "  pkgf             Find which package owns a file"
-        echo "  install          Install and sync or update packages"
-        echo "  add              Install package without sync"
-        echo "  sync             Sync all repos"
-        echo "  update           Update the system and installed packages"
-        echo "  upgrade          Upgrade all installed pacakges"
-        echo "  remove           Remove packages and their dependencies"
-        echo "  search           Search for packages"
-        echo "  info             Show package information"
-        echo "  locate           Locate a package's files"
-        echo "  list             List installed packages"
-        echo "  hold             Hold a package to prevent updates"
-        echo "  unhold           Unhold a package to allow updates"
-        echo "  mirror           Update XBPS mirror list"
-        echo "  kill             Kill all processes matching a pattern"
-        echo "  clean-cache      Remove cached packages"
-        echo "  clear-cache      Clean all cached packages"
-        echo "  services         List services in /etc/sv/"
-        echo "  active-services  List services in /var/service/"
-        echo "  restart          Restart a service"
-        echo "  status           Show the status of a service"
-        echo "  start            Start a service"
-        echo "  stop             Stop a service"
+        _xbps_display_help
         return 0
     end
 
+
+    set -l sub_command $argv[1]
+    set -l cmd_args $argv[2..-1]
+
     switch $sub_command
         case pkgs
-            cd $VOID_PACKAGES_PATH
+            _xbps_change_to_packages_dir
 
         case shutdown
             sudo shutdown -h now
@@ -48,13 +24,13 @@ function xbps --argument-names sub_command -d "Short and friendly command wrappe
             xbps-query -L
 
         case pkgf
-            xbps-query -f $argv[2..-1]
+            _xbps_validate_args $cmd_args && xbps-query -f $cmd_args
 
         case install
-            sudo xbps-install -S $argv[2..-1]
+            sudo xbps-install -S $cmd_args
 
         case add
-            sudo xbps-install $argv[2..-1]
+            sudo xbps-install $cmd_args
 
         case sync
             sudo xbps-install -S
@@ -63,34 +39,34 @@ function xbps --argument-names sub_command -d "Short and friendly command wrappe
             sudo xbps-install -Su
 
         case update
-            sudo xbps-install -Su $argv[2..-1]
+            sudo xbps-install -Su $cmd_args
 
         case remove
-            sudo xbps-remove -R $argv[2..-1]
+            sudo xbps-remove -R $cmd_args
 
         case search
-            xbps-query -Rs $argv[2..-1]
+            xbps-query -Rs $cmd_args
 
         case info
-            xbps-query -S $argv[2..-1]
+            xbps-query -S $cmd_args
 
         case locate
-            xbps-query -f $argv[2..-1]
+            xbps-query -f $cmd_args
 
         case list
             xbps-query -l
 
         case hold
-            sudo xbps-pkgdb -m hold $argv[2..-1]
+            _xbps_validate_args $cmd_args && sudo xbps-pkgdb -m hold $cmd_args
 
         case unhold
-            sudo xbps-pkgdb -m unhold $argv[2..-1]
+            _xbps_validate_args $cmd_args && sudo xbps-pkgdb -m unhold $cmd_args
 
         case mirror
             sudo xmirror
 
         case kill
-            pkill -f $argv[2..-1]
+            _xbps_validate_args $cmd_args && pkill -f $cmd_args
 
         case clean-cache
             sudo xbps-remove -O
@@ -105,42 +81,101 @@ function xbps --argument-names sub_command -d "Short and friendly command wrappe
             ls -la /var/service/
 
         case restart
-            sudo sv restart $argv[2..-1]
+            _xbps_validate_args $cmd_args && sudo sv restart $cmd_args
 
         case status
-            sudo sv status $argv[2..-1]
+            _xbps_validate_args $cmd_args && sudo sv status $cmd_args
 
         case start
-            sudo sv up $argv[2..-1]
+            _xbps_validate_args $cmd_args && sudo sv up $cmd_args
 
         case stop
-            sudo sv down $argv[2..-1]
+            _xbps_validate_args $cmd_args && sudo sv down $cmd_args
 
         case enable
-            if not set -q argv[2]
-                echo "Error: Please provide the name of the service to enable"
-
-                return 1
-            end
-
-            for service in $argv[2..-1]
-                sudo ln -s /etc/sv/$service /var/service
-            end
+            _xbps_manage_service enable $cmd_args
 
         case disable
-            if not set -q argv[2]
-                echo "Error: Please provide the name of the service to remove"
-
-                return 1
-            end
-
-            for service in $argv[2..-1]
-                sudo rm -rf /var/service/$service
-            end
-
+            _xbps_manage_service disable $cmd_args
 
         case '*'
-            echo "Unknown command: $argv[1]"
+            echo "Unknown command: $sub_command"
             echo "Use 'xbps --help' to see the list of available commands."
+            return 1
+    end
+end
+
+function _xbps_display_help
+    echo "Usage: xbps COMMAND [OPTIONS] [arg...]"
+    echo "Commands:"
+    set -l commands \
+        "pkgs:Change directory to VOID_PACKAGES_PATH" \
+        "shutdown:Shutdown the system immediately" \
+        "reboot:Reboot the system immediately" \
+        "repos:List XBPS repositories" \
+        "pkgf:Find which package owns a file" \
+        "install:Install and sync or update packages" \
+        "add:Install package without sync" \
+        "sync:Sync all repos" \
+        "update:Update the system and installed packages" \
+        "upgrade:Upgrade all installed packages" \
+        "remove:Remove packages and their dependencies" \
+        "search:Search for packages" \
+        "info:Show package information" \
+        "locate:Locate a package's files" \
+        "list:List installed packages" \
+        "hold:Hold a package to prevent updates" \
+        "unhold:Unhold a package to allow updates" \
+        "mirror:Update XBPS mirror list" \
+        "kill:Kill all processes matching a pattern" \
+        "clean-cache:Remove cached packages" \
+        "clear-cache:Clean all cached packages" \
+        "services:List services in /etc/sv/" \
+        "active-services:List services in /var/service/" \
+        "restart:Restart a service" \
+        "status:Show the status of a service" \
+        "start:Start a service" \
+        "stop:Stop a service" \
+        "enable:Enable a service" \
+        "disable:Disable a service"
+
+    for cmd in $commands
+        set -l parts (string split ':' $cmd)
+        printf "  %-15s %s\n" $parts[1] $parts[2]
+    end
+end
+
+function _xbps_change_to_packages_dir
+    if set -q VOID_PACKAGES_PATH
+        cd $VOID_PACKAGES_PATH
+    else
+        echo "Error: VOID_PACKAGES_PATH is not set"
+        return 1
+    end
+end
+
+function _xbps_validate_args
+    if not set -q argv[1]
+        echo "Error: At least one argument is required"
+        return 1
+    end
+end
+
+function _xbps_manage_service
+    set -l action $argv[1]
+    set -l services $argv[2..-1]
+
+    if not set -q services[1]
+        echo "Error: Please provide the name of the service to $action"
+        return 1
+    end
+
+    for service in $services
+        switch $action
+            case enable
+                sudo ln -s /etc/sv/$service /var/service
+            case disable
+                sudo rm -rf /var/service/$service
+        end
     end
 end
