bd80ee3e7652ddb15e575105f54890c228f465ae
hesam-init <hesam.init@gmail.com>
2025-06-28 08:25:47 +0330

    fix: hyprland fish shell start

--- shells/fish/conf.d/start.fish
+++ shells/fish/conf.d/start.fish
@@ -1,7 +1,11 @@
 if test (tty) = "/dev/tty1"
     if not set -q HYPRLAND_RUNNING
-        set -g HYPRLAND_RUNNING 1
-        
-        exec dbus-run-session Hyprland
+        if type -q Hyprland
+            set -g HYPRLAND_RUNNING 1
+            
+            exec dbus-run-session Hyprland
+        else
+            echo "Hyprland not found in PATH"
+        end
     end
 end
