0278851c1b1ead6b8cd996152c4d70e098541e39
hesam-init <hesam.init@gmail.com>
2025-07-21 07:24:50 +0330

    chore: new server for speedtest

--- shells/fish/utils/_speedtest.fish
+++ shells/fish/utils/_speedtest.fish
@@ -51,5 +51,5 @@ function speedtest-fish --description 'Download a file from a given URL with an 
     end
 end
 
-alias ir="speedtest-fish RAM AXEL https://mirror.mobinhost.com/.speedtest/10mb.bin"
-alias irwget="speedtest-fish RAM WGET https://mirror.mobinhost.com/.speedtest/10mb.bin"
+alias ir="speedtest-fish RAM AXEL https://repo.iut.ac.ir/repo/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst"
+alias irwget="speedtest-fish RAM WGET https://repo.iut.ac.ir/repo/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst"
