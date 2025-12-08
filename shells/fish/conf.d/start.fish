42fa1111a8cf60146de61a663dd382a9e466b211
hesam-init <hesam.init@gmail.com>
2025-12-08 11:24:56 +0330

    chore: hyprland on startup removed

--- shells/fish/conf.d/start.fish
+++ shells/fish/conf.d/start.fish
@@ -1,11 +1,11 @@
-if test (tty) = "/dev/tty1"
-    if not set -q HYPRLAND_RUNNING
-        if type -q Hyprland
-            set -g HYPRLAND_RUNNING 1
-            
-            exec dbus-run-session Hyprland
-        else
-            echo "Hyprland not found in PATH"
-        end
-    end
-end
+# if test (tty) = "/dev/tty1"
+#     if not set -q HYPRLAND_RUNNING
+#         if type -q Hyprland
+#             set -g HYPRLAND_RUNNING 1
+
+#             exec dbus-run-session Hyprland
+#         else
+#             echo "Hyprland not found in PATH"
+#         end
+#     end
+# end
