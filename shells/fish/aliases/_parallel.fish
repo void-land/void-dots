9857ba560b31de9d665754423ccd3d8b0a150d09
hesam-init <hesam.init@gmail.com>
2026-03-17 11:20:47 +0330

    chore: new fish shortcut

--- shells/fish/aliases/_parallel.fish
+++ shells/fish/aliases/_parallel.fish
@@ -1,2 +1,6 @@
-abbr paxel "cat urls.txt | parallel --keep-order --jobs 8 'axel -n 16 {} 2>/dev/null || grep -n {} urls.txt'"
-abbr pdns "cat ./dnslist.txt | parallel --keep-order --jobs 80 'dig @{} -p 53 +short +retry=0 +timeout=1 google.com >/dev/null && echo {}'"
+abbr pfiles "ls | parallel 'echo -n {} = ; ls {} | wc -l'"
+
+abbr paxel "cat urls.txt | parallel --keep-order --jobs 8 'axel -n 16 {} --quiet'"
+abbr pwget "cat urls.txt | parallel --keep-order --jobs 8 'wget --content-disposition -c {}'"
+
+abbr pdns "cat ./dnslist.txt | parallel -X --keep-order --jobs 80 'dig @{} -p 53 +short +retry=0 +timeout=1 google.com >/dev/null && echo {}'"
