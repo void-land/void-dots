711b06dea08af649bac078d28431fc9e729bef78
hesam-init <hesam.init@gmail.com>
2026-05-27 20:33:10 +0330

    chore: fish abbreviations updated

--- shells/fish/aliases/_fish.fish
+++ shells/fish/aliases/_fish.fish
@@ -1,2 +1,2 @@
 abbr refish "source ~/.config/fish/config.fish"
-abbr cfish "find ~/.config/fish/ -maxdepth 1 -name 'fish_variables*' -not -name 'fish_variables' -delete"
+abbr cfish "find ~/.config/fish/ -maxdepth 1 -name 'fish_variables*' -not -name 'fish_variables' -delete -print"
---
+++ shells/fish/aliases/_magic.fish
@@ -0,0 +1,1 @@
+abbr cfdns "curl -sSLf --resolve one.one.one.one:40443:127.0.0.1 https://one.one.one.one:40443/ | grep '^\.\.'"
